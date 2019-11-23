df7292e6bf66b9c8190f9ade28b33375f40bfdc4
jboe26 <joshrboepple@outlook.com>
2019-11-23 09:01:58 -0500

    testing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = 3001;
 
 // Initialize Express
 var app = express();
@@ -33,7 +33,7 @@ mongoose.connect("mongodb://localhost/Scraper", { useNewUrlParser: true });
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.cnn.com/").then(function(response) {
+  axios.get("http://www.reddit.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
